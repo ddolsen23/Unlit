3495dc492ac11ff384ec94ce04b12f5c5803ff6f
Jon Wiedmann <jonwiedmann@gmail.com>
2014-11-03 19:44:27 -0500

    Add MagicalCharacter

---
+++ Source/Unlit/MagicalCharacter.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Unlit.h"
+#include "MagicalCharacter.h"
+
+
+AMagicalCharacter::AMagicalCharacter(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/Unlit/MagicalCharacter.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "MagicalCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNLIT_API AMagicalCharacter : public ACharacter
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
